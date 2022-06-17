8ffcb088f8355e404c992001bd0f46732285f45d
Pavel <p21011982n@yandex.ru>
2022-06-17 11:46:18 +0300

    Асинхронно передаем вызов в интернет

--- app/src/main/java/com/android/retrofitsampleapp2/MainActivity.java
+++ app/src/main/java/com/android/retrofitsampleapp2/MainActivity.java
@@ -1,9 +1,15 @@
 package com.android.retrofitsampleapp2;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,8 +28,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //вызываем проект (репозиторий) кнкретного пользователя. Вызов идет ассинхронно в том же потоке
-        // (enqueue - ставить в очередь в () передаем коллбэк, execute - выполнять).
-        gitHubApi.getProject("borhammere");
+        //вызываем проект (репозиторий) конкретного пользователя. Вызов идет ассинхронно в том же потоке
+        // (enqueue - ставить в очередь в () передаем Callback, execute - выполнять).
+        // Callback состоит из двух методов: onResponse - получение ответа; onFailure - получение ошибки (нет сети ошибка сервера, DNS и т.д.).
+        gitHubApi.getProject("borhammere").enqueue(new Callback<List<GitProjectEntity>>() {
+            @Override
+            public void onResponse(Call<List<GitProjectEntity>> call, Response<List<GitProjectEntity>> response) {
+//                if (response.code()==200){  //проверка кода, код 200 означает успех
+                    if (response.isSuccessful()) { //isSuccessful - это уже проверка кодов от 200 до 300
+                        List<GitProjectEntity> projects = response.body(); // body - это тело запроса, это будет список репозиториев которые мы ищем. Здесь мы получаем список проектов
+
+                    } else {
+                        Toast.makeText(MainActivity.this, "Error code" + response.code(), Toast.LENGTH_LONG).show();
+                    }
+            }
+
+            @Override
+            public void onFailure(Call<List<GitProjectEntity>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
