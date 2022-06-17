148a99cf04c13216928b7260ec0ccd39a33115c4
Pavel <p21011982n@yandex.ru>
2022-06-17 11:05:39 +0300

    Пример (теория) вызова списка проектов конкретного пользователя.

--- app/src/main/java/com/android/retrofitsampleapp2/MainActivity.java
+++ app/src/main/java/com/android/retrofitsampleapp2/MainActivity.java
@@ -1,14 +1,20 @@
 package com.android.retrofitsampleapp2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    GitHubApi gitHubApi = null; // создали gitHubApi для обращения к нему
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //вызываем проект (репозиторий) кнкретного пользователя. Вызов идет ассинхронно в том же потоке
+        // (enqueue - ставить в очередь в () передаем коллбэк, execute - выполнять).
+        gitHubApi.getProject("borhammere").enqueue();
     }
 }
