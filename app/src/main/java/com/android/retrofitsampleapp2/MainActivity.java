53f18e01f76a00f3e451efd19b68a5663c6f4ca5
Pavel <p21011982n@yandex.ru>
2022-06-17 11:24:13 +0300

    Создаем объект Retrofit и создаем нужный тип данных (GitHubApi.class).

--- app/src/main/java/com/android/retrofitsampleapp2/MainActivity.java
+++ app/src/main/java/com/android/retrofitsampleapp2/MainActivity.java
@@ -4,9 +4,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import retrofit2.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
 
-    GitHubApi gitHubApi = null; // создали gitHubApi для обращения к нему
+    //создаем Retrofit
+    private final Retrofit retrofit = new Retrofit.Builder()
+            //это начало адреса которая будет подставлятся для открытия списка пользователя и их репозитория
+            .baseUrl("https://api.github.com/")
+            .build();
+
+    // создали gitHubApi для обращения к нему (create - творить). GitHubApi.class - тип данных который нужно создать
+    private GitHubApi gitHubApi = retrofit.create(GitHubApi.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
         //вызываем проект (репозиторий) кнкретного пользователя. Вызов идет ассинхронно в том же потоке
         // (enqueue - ставить в очередь в () передаем коллбэк, execute - выполнять).
-        gitHubApi.getProject("borhammere").enqueue();
+        gitHubApi.getProject("borhammere");
     }
 }
