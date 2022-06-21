8b10723cc675026afb52a90c4d94c677aaa71b9b
Pavel <p21011982n@yandex.ru>
2022-06-21 17:29:17 +0300

    Создали второе окно ProjectsActivity

---
+++ app/src/main/java/com/android/retrofitsampleapp2/iu/ProjectsActivity.java
@@ -0,0 +1,17 @@
+package com.android.retrofitsampleapp2.iu;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.retrofitsampleapp2.R;
+
+public class ProjectsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projects);
+    }
+}
