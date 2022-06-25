2b92d801845d23aff5ec5052d644e02a70c0cf8e
Pavel <p21011982n@yandex.ru>
2022-06-25 12:13:41 +0300

    Подготовка к передаче данных пользователя

--- app/src/main/java/com/android/retrofitsampleapp2/iu/ProjectsActivity.java
+++ app/src/main/java/com/android/retrofitsampleapp2/iu/ProjectsActivity.java
@@ -3,16 +3,24 @@ package com.android.retrofitsampleapp2.iu;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.retrofitsampleapp2.R;
 
 public class ProjectsActivity extends AppCompatActivity {
 
     private static final String LOGIN_EXTRA_KEY = "LOGIN_EXTRA_KEY";
+
+    private final GitProjectAdapter adapter = new GitProjectAdapter();
+    private ProgressBar progressBar;
+    private RecyclerView recyclerView;
+
 
     public static Intent getLaunchIntent(Context context, String login) {
         Intent intent = new Intent(context, ProjectsActivity.class);
@@ -25,7 +33,31 @@ public class ProjectsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projects);
 
-        final String login = getIntent().getStringExtra(LOGIN_EXTRA_KEY);
-        Toast.makeText(this, login, Toast.LENGTH_SHORT).show();
+        initView();
+
+        final String login = getIntent().getStringExtra(LOGIN_EXTRA_KEY);//получаем логин
+
+        loadProjects(login);
+    }
+
+    private void loadProjects(String login) {
+
+    }
+
+    private void initView() {
+        progressBar = findViewById(R.id.progress_bar);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void showProgress(boolean shouldShow) {
+        if (shouldShow) {
+            recyclerView.setVisibility(View.GONE);//скрываем view со списком
+            progressBar.setVisibility(View.VISIBLE);//показываем прогресс загрузки
+        } else {
+            recyclerView.setVisibility(View.VISIBLE);//показываем view со списком
+            progressBar.setVisibility(View.GONE);//скрываем прогресс загрузки
+        }
     }
 }
