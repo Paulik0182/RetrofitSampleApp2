c373ad5ba1e3c7848bc51fa100e45c115caa3f5f
Pavel <p21011982n@yandex.ru>
2022-07-08 16:38:21 +0300

    translation -> GitProjectEntity

--- app/src/main/java/com/android/retrofitsampleapp2/domain/GitProjectEntity.kt
+++ app/src/main/java/com/android/retrofitsampleapp2/domain/GitProjectEntity.kt
@@ -1,38 +1,7 @@
-package com.android.retrofitsampleapp2.domain;
+package com.android.retrofitsampleapp2.domain
 
-public class GitProjectEntity {
-
-    private int id;
-    private String name;
-    private String description;
-
-    public GitProjectEntity(int id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-}
+data class GitProjectEntity(
+    var id: Int,
+    var name: String,
+    var description: String
+)
