bb880fad2a201047dd0f2d9f8d1161f4670c49e6
Pavel <p21011982n@yandex.ru>
2022-07-08 16:29:48 +0300

    translation -> App

--- app/src/main/java/com/android/retrofitsampleapp2/App.kt
+++ app/src/main/java/com/android/retrofitsampleapp2/App.kt
@@ -1,31 +1,23 @@
-package com.android.retrofitsampleapp2;
+package com.android.retrofitsampleapp2
 
-import android.app.Application;
+import android.app.Application
+import com.android.retrofitsampleapp2.data.GitHubApi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
-import com.android.retrofitsampleapp2.data.GitHubApi;
-
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.OkHttpClient;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-public class App extends Application {
+class App : Application() {
     //увеличили время по таймауту при загрузке из сети (без этого, по умолчанию 10сек.)
-    private final OkHttpClient client = new OkHttpClient.Builder()
-            .connectTimeout(15, TimeUnit.SECONDS)
-            .build();
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(15, TimeUnit.SECONDS)
+        .build()
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .client(client) // увеличение времени по таймауту
+        .addConverterFactory(GsonConverterFactory.create()) // это приобразователь объектов из одного типа в другой тип (здесь старонняя библиотека)
+        .build()
 
-    private final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://api.github.com/")
-            .client(client)// увеличение времени по таймауту
-            .addConverterFactory(GsonConverterFactory.create())// это приобразователь объектов из одного типа в другой тип (здесь старонняя библиотека)
-            .build();
-
-    private final GitHubApi gitHubApi = retrofit.create(GitHubApi.class); //создаем gitHubApi. Автоматически обратится к интерфейсу
-
-    //метод для
-    public GitHubApi getGitHubApi() {
-        return gitHubApi;
-    }
+    val gitHubApi =
+        retrofit.create(GitHubApi::class.java) //создаем gitHubApi. Автоматически обратится к интерфейсу
 }
