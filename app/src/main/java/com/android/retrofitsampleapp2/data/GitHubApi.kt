2051aa685efcd32f5f891cc0fbc073f2bcbed145
Pavel <p21011982n@yandex.ru>
2022-07-08 16:36:40 +0300

    translation -> GitHubApi

--- app/src/main/java/com/android/retrofitsampleapp2/data/GitHubApi.kt
+++ app/src/main/java/com/android/retrofitsampleapp2/data/GitHubApi.kt
@@ -1,32 +1,29 @@
-package com.android.retrofitsampleapp2.data;
+package com.android.retrofitsampleapp2.data
 
-import com.android.retrofitsampleapp2.domain.GitProjectEntity;
-import com.android.retrofitsampleapp2.domain.GitUserEntity;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
+import com.android.retrofitsampleapp2.domain.GitProjectEntity
+import com.android.retrofitsampleapp2.domain.GitUserEntity
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 /**
  * Здесь мы описываем простейшее App.
  * Call - это звонок. То-есть мы получаем список сущьностей.
  * GET("users") это анатации которые потом превращает интерфейс в реальные объекты. это GET запрос.
  * Path("user") String user) - это переменная которая является частью пути.
- * <p>
+ *
+ *
  * GitUserEntity - класс описывающий сущность, пользователя
- * <p>
+ *
+ *
  * GitProjectEntity - класс описывающий сущность (проект, репозиторий)
  */
-
-public interface GitHubApi {
-
+interface GitHubApi {
     //здесь мы получем список всех пользователей. Результат - https://api.github.com/users
     @GET("users")
-    Call<List<GitUserEntity>> getUsers();
+    fun getUsers(): Call<List<GitUserEntity>>
 
     //это конкретный запрос на репозиторий. Результат - https://api.github.com/users/borhammere/repos
     @GET("users/{user}/repos")
-    Call<List<GitProjectEntity>> getProject(@Path("user") String user);
+    fun getProject(@Path("user") user: String): Call<List<GitProjectEntity>>
 }
